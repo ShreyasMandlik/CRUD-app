0b81d07956011ab83064b5752bc9b7adfca63af0
Shreyas <mandlikshreyas01@gmail.com>
2022-11-03 23:07:41 +0530

    Hosting

--- Todo_List/settings.py
+++ Todo_List/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1','listodo1.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'Todo_List.urls'
 
@@ -119,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES_DIR=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Todo_List/urls.py
+++ Todo_List/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.views.static import serve
-from django.urls import re_path as url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('List.urls')),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
